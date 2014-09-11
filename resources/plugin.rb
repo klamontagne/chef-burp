0e4950d649a5cb5d250cba66d01da9b0e7649f3d
Kevin Lamontagne <kevin@demarque.com>
2014-09-10 20:09:26 -0400

    Make pre_template and post_template nullable

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -3,5 +3,5 @@ default_action :create
 
 attribute :name, kind_of: String, name_attribute: true
 attribute :cookbook, kind_of: String, required: true
-attribute :pre_template, kind_of: String, default: 'pre_backup.sh'
-attribute :post_template, kind_of: String, default: 'post_backup.sh'
+attribute :pre_template, kind_of: [String, NilClass], default: 'pre_backup.sh'
+attribute :post_template, kind_of: [String, NilClass], default: 'post_backup.sh'
