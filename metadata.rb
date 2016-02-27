2ee45064d387d84e0c25aa9c934f18732de5447f
kelam <kevinlamontagne@gmail.com>
2016-02-27 15:43:01 -0500

    Bump version -> 0.2.3

--- metadata.rb
+++ metadata.rb
@@ -3,17 +3,15 @@ maintainer 'Kevin Lamontagne'
 maintainer_email 'kevinlamontagne@gmail.com'
 license 'MIT'
 description <<-EOF
-Installs & configures a [BURP](http://burp.grke.org/) backup server and
-its clients. Adds
-[definitions](https://docs.getchef.com/essentials_cookbook_definitions.html) to
-add paths to backup or exclude, and "plugin" scripts to execute before and/or
+Installs & configures a BURP ( http://burp.grke.org/ ) backup server and
+its clients. Adds resources to backup or exclude paths, and "plugin" scripts to execute before and/or
 after a backup. (such as dumping a database)
 EOF
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url "https://github.com/klamontagne/chef-burp" if respond_to?(:source_url)
 issues_url "https://github.com/klamontagne/chef-burp/issues" if respond_to?(:issues_url)
 
-version '0.2.2'
+version '0.2.3'
 
 supports 'ubuntu', '>= 12.04'
 
